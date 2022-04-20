46c46e0eff1134c1fefc90acebcdcc4c2d02b275
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-04-20 09:40:01 +0100

    Create first my scene

--- src/main/java/com/javafx/App.java
+++ src/main/java/com/javafx/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,9 +19,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(btn);
+        scene = new Scene(stackPane, 300, 250);
         stage.setScene(scene);
         stage.show();
+        // scene = new Scene(loadFXML("primary"), 640, 480);
+        // stage.setScene(scene);
+        // stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
