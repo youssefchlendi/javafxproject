6247f31d61b46e46a04395884683f1eda932a816
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-16 00:49:11 +0100

    complete atelier0

--- Atelier0/src/main/java/com/javafx/App.java
+++ Atelier0/src/main/java/com/javafx/App.java
@@ -1,12 +1,15 @@
 package com.javafx;
-
+import javafx.event.ActionEvent;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -24,18 +27,39 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         VBox vBox = new VBox();
+        HBox hb1 = new HBox();
 
         Scene scene = new Scene(vBox);
-        stage.setScene(scene);
-        stage.show();
+
+        hb1.setSpacing(10);
+        hb1.setPadding(new Insets(20));
+
 
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        Label label = new Label("Hello World");
+        Label label = new Label("Enter your name: ");
         TextField textField = new TextField();
-        vBox.getChildren().addAll(label,textField);
+        hb1.getChildren().addAll(label, textField);
 
-        
+        HBox hb2 = new HBox();
+        hb2.setSpacing(10);
+        hb2.setPadding(new Insets(20));
+        Button button = new Button("Say Hello! ");
+        Label label2 = new Label();
 
+            // Button action using EventHandler
+        // button.setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent event) {
+        //         label2.setText("Hello "+textField.getText());
+        //     }
+        // });
+            // Buton action using lambda expression
+        button.setOnAction(event -> label2.setText("Hello "+textField.getText()));
+        hb2.getChildren().addAll(button, label2);
+        vBox.getChildren().addAll(hb1, hb2);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
