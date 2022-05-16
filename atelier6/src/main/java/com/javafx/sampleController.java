e5c988a49eefe78ccf3287b74529b1caa702bfe3
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-16 13:04:43 +0100

    add calculate square

---
+++ atelier6/src/main/java/com/javafx/sampleController.java
@@ -0,0 +1,35 @@
+package com.javafx;
+
+import java.io.IOException;
+import java.util.function.Function;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+
+public class sampleController {
+
+    @FXML
+    TextField txt;
+
+    @FXML
+    Label lbl1;
+
+
+    @FXML
+    public void Calculate() {
+        try{
+            int a = Integer.parseInt(txt.getText());
+            int b = a * a;
+            lbl1.setText("Carré = " +String.valueOf(b));
+        }catch(Exception e){
+            lbl1.setText("Error Pleaase enter a num");
+        }
+    }
+}
