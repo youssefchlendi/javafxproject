4c90044bdb40dac3ac3f28ee83f08264723f75a0
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-16 01:13:26 +0100

    use sample.fxml and style.css

--- Atelier1/src/main/java/com/javafx/App.java
+++ Atelier1/src/main/java/com/javafx/App.java
@@ -1,51 +1,32 @@
 package com.javafx;
-import javafx.event.ActionEvent;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        VBox vBox = new VBox();
-        HBox hb1 = new HBox();
-
-        vBox.getChildren().add(hb1);
-        Scene scene = new Scene(vBox);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
