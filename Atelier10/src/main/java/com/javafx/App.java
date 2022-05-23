355cf2e0807a58a5cb849d8a9152bbd0aa3e2de5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-05-23 14:04:21 +0100

    add and delete

--- Atelier10/src/main/java/com/javafx/App.java
+++ Atelier10/src/main/java/com/javafx/App.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
